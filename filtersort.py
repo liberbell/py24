e1c3553c09a163b0f33f3b56bb16381ab2de8972
Liberbell <liberbell@gmail.com>
2020-07-27 12:01:27 +0900

    filtersort4

--- filtersort.py
+++ filtersort.py
@@ -6,4 +6,9 @@ work_sheet = work_book.active
 data = [["Champion", "Year"],
         ["France", 2018],
         ["Spain", 2010],
-        ["Italy", 2006]]
+        ["Italy", 2006],
+        ["France", 1998],
+        ["Brazil", 1994],
+        ["Argentina", 1986],
+        ["Italy", 1982],
+        ["Argentina", 1978]]
