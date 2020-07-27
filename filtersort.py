f67fa705f73d65d6eba95c3d4cf27650fcdbb61b
Liberbell <liberbell@gmail.com>
2020-07-28 07:52:55 +0900

    filtersort5

--- filtersort.py
+++ filtersort.py
@@ -32,3 +32,6 @@ for r in data:
 # work_book.save("world_cup_winners.xlsx")
 
 print(work_sheet.calculate_dimension())
+
+work_sheet.auto_filter.fef = work_sheet.calculate_dimension()
+work_sheet.auto_filter.add_filter_column(0, ["Brazil", "Italy", "Argentina"])
