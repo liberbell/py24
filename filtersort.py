2dce3bfcf231d5e7f06610fc7dcfd735af2f048e
Liberbell <liberbell@gmail.com>
2020-07-27 12:00:10 +0900

    filtersort3

--- filtersort.py
+++ filtersort.py
@@ -2,3 +2,8 @@ import openpyxl
 
 work_book = openpyxl.Workbook()
 work_sheet = work_book.active
+
+data = [["Champion", "Year"],
+        ["France", 2018],
+        ["Spain", 2010],
+        ["Italy", 2006]]
