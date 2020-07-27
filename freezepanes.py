6f412f513cb0a4823ff849c822e0f2359a18875d
Liberbell <liberbell@gmail.com>
2020-07-27 11:51:09 +0900

    freezepanes2

--- freezepanes.py
+++ freezepanes.py
@@ -4,4 +4,7 @@ work_book = openpyxl.load_workbook("zomato-reviews.xlsx")
 sheet = work_book.active
 
 sheet.freeze_panes = "A2"
+# work_book.save("zomato-reviews.xlsx")
+
+sheet.freeze_panes = "B1"
 work_book.save("zomato-reviews.xlsx")
