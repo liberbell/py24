332d2e93d8203df630ec6c21200055147a4a256d
Liberbell <liberbell@gmail.com>
2020-07-29 08:32:36 +0900

    resize4

--- resize.py
+++ resize.py
@@ -6,3 +6,8 @@ sheet_obj = work_book.active
 print(sheet_obj)
 sheet_obj.title = "FirstSheet"
 print(sheet_obj)
+
+sheet_obj["C1"] = "A high row"
+sheet_obj["D4"] = "A wide column"
+
+sheet_obj.row_dimensions[1].height = 70
