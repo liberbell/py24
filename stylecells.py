3b3ed51876c976eb69a887e56dba9453bb187e35
Liberbell <liberbell@gmail.com>
2020-08-01 09:34:25 +0900

    stylecells3

--- stylecells.py
+++ stylecells.py
@@ -1,4 +1,7 @@
 import openpyxl
+from openpyxl.styles import Font, Color, Alignment, Border, Side, colors
 
 work_book = openpyxl.load_workbook("student_data.xlsx")
 print(work_book.sheetnames)
+
+sheet = work_book["Sheet"]
