96030944364a7a95cb7e94e4523e1baa67d584b2
Liberbell <liberbell@gmail.com>
2020-08-04 07:41:46 +0900

    conformat4

--- conformat.py
+++ conformat.py
@@ -4,3 +4,8 @@ work_book = openpyxl.load_workbook("sales_record.xlsx")
 sheet = work_book.active
 
 print(sheet.max_row)
+print(sheet.max_column)
+
+for row in sheet["K2:M101"]:
+    for cell in row:
+        cell.number_format = "#,##0"
