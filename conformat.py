f99d747e5f72f34a99661a2707f749e4b115d59e
Liberbell <liberbell@gmail.com>
2020-08-04 07:43:40 +0900

    conformat5

--- conformat.py
+++ conformat.py
@@ -6,6 +6,6 @@ sheet = work_book.active
 print(sheet.max_row)
 print(sheet.max_column)
 
-for row in sheet["K2:M101"]:
+for row in sheet["L2:N101"]:
     for cell in row:
         cell.number_format = "#,##0"
