9da151aa93f8ac1fe85bcacf1fc8077d02e50a67
Liberbell <liberbell@gmail.com>
2020-08-07 08:58:00 +0900

    conditionformat2

--- conditionformat.py
+++ conditionformat.py
@@ -1,6 +1,7 @@
 import openpyxl
 from openpyxl.formatting.rule import IconSetRule
 
-work_book = openpyxl.load_workbook("zomato_reviews.xlsx")
+# work_book = openpyxl.load_workbook("zomato_reviews.xlsx")
+work_book = openpyxl.load_workbook("zomato-reviews.xlsx")
 
 sheet = work_book.active
