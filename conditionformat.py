ae028f0b21113dbcf226c2697890e683fa4001d0
Liberbell <liberbell@gmail.com>
2020-08-07 09:05:30 +0900

    conditionformat5

--- conditionformat.py
+++ conditionformat.py
@@ -10,3 +10,4 @@ icon_set_rule = IconSetRule(icon_style="4Arrows", type="num", values=[1, 2, 3, 4
 print(sheet.max_row)
 
 sheet.conditional_formatting.add("G2:G9558", icon_set_rule)
+work_book.save("zomato_iconset.xlxs")
