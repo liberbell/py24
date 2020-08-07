45e67852ea24947d14ed69e3e2e907c716c3ed57
Liberbell <liberbell@gmail.com>
2020-08-07 09:19:43 +0900

    conditionformat9

--- conditionformat.py
+++ conditionformat.py
@@ -1,5 +1,5 @@
 import openpyxl
-from openpyxl.formatting.rule import IconSetRule
+from openpyxl.formatting.rule import IconSetRule, DataBarRule 
 
 # work_book = openpyxl.load_workbook("zomato_reviews.xlsx")
 work_book = openpyxl.load_workbook("zomato-reviews.xlsx")
@@ -10,4 +10,9 @@ icon_set_rule = IconSetRule(icon_style="4Arrows", type="num", values=[1, 2, 3, 4
 print(sheet.max_row)
 
 sheet.conditional_formatting.add("Q2:Q9558", icon_set_rule)
-work_book.save("zomato_iconset.xlsx")
+# work_book.save("zomato_iconset.xlsx")
+
+work_book = openpyxl.load_workbook("zomato-reviews.xlsx")
+sheet = work_book.active
+
+data_bar_rule = DataBarRule(start_type="num", start_value=1, end_value="num", end_value="4", color="ff0000")
