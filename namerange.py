ad4369ba669bd123925dbddd55faba6b3bd8e42e
Liberbell <liberbell@gmail.com>
2020-08-13 10:18:38 +0900

    namerange2

--- namerange.py
+++ namerange.py
@@ -6,3 +6,9 @@ sheet = work_book["Products"]
 fx_range = work_book.defined_names["fx_rates"]
 print(fx_range)
 print(fx_range.destinations)
+
+cells = []
+
+for title, coord in fx_range.destinations:
+    ws = work_book[title]
+    cells.append(ws[coord])
