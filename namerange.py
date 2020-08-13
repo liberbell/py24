98d8c50b6f036da952ce35e3055e05692711c987
Liberbell <liberbell@gmail.com>
2020-08-13 10:36:44 +0900

    namerange9

--- namerange.py
+++ namerange.py
@@ -32,3 +32,5 @@ for row in sheet["E3:E" + max_row_str]:
     for cell in row:
         cell.value = "=$B${0}*VLOOKUP($E$2, fx_rates, 2, False)".format(cell.row)
         cell.number_format = "#,##0,00"
+
+work_book.save("named_range.xlsx")
