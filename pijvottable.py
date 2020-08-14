c22b0f038dd9dbc4e498c43fc54df44991f730ca
Liberbell <liberbell@gmail.com>
2020-08-14 16:45:41 +0900

    pivottable8

--- pijvottable.py
+++ pijvottable.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import numpy as np
 
 sales_df = pd.read_excel("sales_record.xlsx")
 print(sales_df.head())
 
 print(sales_df.sort_values(by = ["Region", "Country", "Item Type"]).head(10))
 print(sales_df.sort_values(by = ["Region", "Country", "Item Type"]).tail(10))
+
+table = pd.pivot_table(sales_df,
+                       index=["Region", "Country"],
+                       values=["Units_Sold", "Total_Revenue", "Total_Profit"],
+                       aggfunc=[np.sum])
