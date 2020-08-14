5c1de9a5b9c7a729fabb4f570a618dc0be4697b4
Liberbell <liberbell@gmail.com>
2020-08-14 16:41:20 +0900

    pivottable7

--- pijvottable.py
+++ pijvottable.py
@@ -4,3 +4,4 @@ sales_df = pd.read_excel("sales_record.xlsx")
 print(sales_df.head())
 
 print(sales_df.sort_values(by = ["Region", "Country", "Item Type"]).head(10))
+print(sales_df.sort_values(by = ["Region", "Country", "Item Type"]).tail(10))
